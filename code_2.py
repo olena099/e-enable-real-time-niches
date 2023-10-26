1c4099973f36b76a0a1ffb3d4beb7d38b6ec8309
olena099 <142909583+olena099@users.noreply.github.com>
2023-10-25 20:41:07 -0700

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,55 @@
+def main():
+
+
+    for item in data:
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+import random
+    main()
+def generate_random_data():
+
+    return data    });
+    data.forEach(item => {
+    }
+        console.log("Random Number: " + item);
+    const data = [];
+function generateRandomData() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    for (let i = 0; i < 10; i++) {
+
+    return data;
+    const data = generateRandomData();
+main();
+
+function main() {
+}        for (int i = 0; i < 10; i++) {
+    }
+            System.out.println("Random Number: " + item);
+
+        for (int item : data) {
+        }
+        int[] data = new int[10];
+        }
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+public class RandomDataGenerator {def main():
+if __name__ == "__main__":
+import random
+    main()
+
+        print(f"Random Number: {item}")
+    return data
+
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
+    for item in data:
